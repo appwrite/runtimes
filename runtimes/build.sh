7b96404a09d9c24a62410842a4f37634edc11e6d
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-04-21 11:54:03 +0200

    fix: remove deno 1.2

--- runtimes/build.sh
+++ runtimes/build.sh
@@ -1,8 +1,5 @@
 echo 'Starting build...'
 
-echo 'Deno 1.2...'
-docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/386,linux/ppc64le -t appwrite/env-deno-1.2:1.0.0 ./docker/environments/deno-1.2/ --push
-
 echo 'Deno 1.5...'
 docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/386,linux/ppc64le -t appwrite/env-deno-1.5:1.0.0 ./docker/environments/deno-1.5/ --push
 
