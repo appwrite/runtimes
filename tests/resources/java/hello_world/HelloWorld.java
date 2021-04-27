fe29b29cbfc99069bf408860f2a48531753127a0
Damodar Lohani <dlohani48@gmail.com>
2021-04-27 15:38:06 +0545

    updating test functions for java and kotlin

--- tests/resources/java/hello_world/HelloWorld.java
+++ tests/resources/java/hello_world/HelloWorld.java
@@ -1,1 +1,12 @@
-public class HelloWorld { public static void main(String[ ] args) { System.out.println("Hello Java");  }}
+public class HelloWorld { 
+    public static void main(String[ ] args) {
+        System.out.println(System.getenv("APPWRITE_FUNCTION_ID"));
+        System.out.println(System.getenv("APPWRITE_FUNCTION_NAME"));
+        System.out.println(System.getenv("APPWRITE_FUNCTION_TAG"));
+        System.out.println(System.getenv("APPWRITE_FUNCTION_TRIGGER"));
+        System.out.println(System.getenv("APPWRITE_FUNCTION_ENV_NAME"));
+        System.out.println(System.getenv("APPWRITE_FUNCTION_ENV_VERSION"));
+        System.out.println(System.getenv("APPWRITE_FUNCTION_EVENT"));
+        System.out.println(System.getenv("APPWRITE_FUNCTION_EVENT_DATA"));
+    }
+}
--- tests/resources/kotlin/hello_world/HelloWorld.kt
+++ tests/resources/kotlin/hello_world/HelloWorld.kt
@@ -1,3 +1,10 @@
 fun main() {
-    println("Hello, World!")
+    println(System.getenv("APPWRITE_FUNCTION_ID") ?: "");
+    println(System.getenv("APPWRITE_FUNCTION_NAME") ?: "");
+    println(System.getenv("APPWRITE_FUNCTION_TAG") ?: "");
+    println(System.getenv("APPWRITE_FUNCTION_TRIGGER") ?: "");
+    println(System.getenv("APPWRITE_FUNCTION_ENV_NAME") ?: "");
+    println(System.getenv("APPWRITE_FUNCTION_ENV_VERSION") ?: "");
+    println(System.getenv("APPWRITE_FUNCTION_EVENT") ?: "");
+    println(System.getenv("APPWRITE_FUNCTION_EVENT_DATA") ?: "");
 }
