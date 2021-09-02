d633a896c4e5c20fd166f4b5461a869b0db2616e
Bradley Schofield <bradley@appwrite.io>
2021-09-02 10:13:23 +0100

    Update Version Names

--- runtimes/build.sh
+++ runtimes/build.sh
@@ -1,16 +1,16 @@
 echo 'Starting build...'
 
 echo 'Deno 1.13...'
-docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/386 -t appwrite/runtime-for-deno:1.13.2 ./runtimes/deno-1.13/ --push
+docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/386 -t deno-runtime:1.13.2 ./runtimes/deno-1.13/ --push
 
 echo 'Node 14.5...'
-docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 -t appwrite/runtime-for-node:14.5 ./runtimes/node-14.5/ --push
+docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 -t node-runtime:14.5 ./runtimes/node-14.5/ --push
 
 echo 'Node 15.5...'
-docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 -t appwrite/runtime-for-node:15.5 ./runtimes/node-15.5/ --push
+docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 -t node-runtime-node:15.5 ./runtimes/node-15.5/ --push
 
 echo 'Node 16...'
-docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 -t appwrite/runtime-for-node:16.0 ./runtimes/node-16.0/ --push
+docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 -t node-runtime:16.0 ./runtimes/node-16.0/ --push
 
 echo 'PHP 8...'
-docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 -t appwrite/runtime-for-php:8.0 ./runtimes/php-8.0/ --push
+docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 -t php-runtime:8.0 ./runtimes/php-8.0/ --push
--- src/Runtimes/Runtimes.php
+++ src/Runtimes/Runtimes.php
@@ -16,19 +16,20 @@ class Runtimes
     public function __construct()
     {
         $node = new Runtime('node', 'Node.js');
-        $node->addVersion('14.5', 'node:14.5-alpine', 'appwrite/runtime-for-node:14.5', [System::X86, System::ARM]);
-        $node->addVersion('15.5', 'node:15.5-alpine', 'appwrite/runtime-for-node:15.5', [System::X86, System::ARM]);
-        $node->addVersion('16.0', 'node:16-alpine', 'appwrite/runtime-for-node:16.0', [System::X86, System::ARM]);
+        $node->addVersion('14.5', 'node:14.5-alpine', 'node-runtime:14.5', [System::X86, System::ARM]);
+        $node->addVersion('15.5', 'node:15.5-alpine', 'node-runtime:15.5', [System::X86, System::ARM]);
+        $node->addVersion('16.0', 'node:16-alpine', 'node-runtime:16.0', [System::X86, System::ARM]);
         $this->runtimes['node'] = $node;
 
         $deno = new Runtime('deno', 'Deno');
-        $deno->addVersion('1.10', 'denoland/deno:alpine-1.10.3', 'appwrite/runtime-for-deno:1.10', [System::X86]);
-        $deno->addVersion('1.11', 'denoland/deno:alpine-1.11.5', 'appwrite/runtime-for-deno:1.11', [System::X86]);
-        $deno->addVersion('1.13', 'denoland/deno:alpine-1.13.2', 'appwrite/runtime-for-deno:1.13', [System::X86]);
+        $deno->addVersion('1.10', 'denoland/deno:alpine-1.10.3', 'deno-runtime:1.10', [System::X86]);
+        $deno->addVersion('1.11', 'denoland/deno:alpine-1.11.5', 'deno-runtime:1.11', [System::X86]);
+        $deno->addVersion('1.13', 'denoland/deno:alpine-1.13.2', 'deno-runtime:1.13', [System::X86]);
         $this->runtimes['deno'] = $deno;
 
         $php = new Runtime('php', 'PHP');
-        $php->addVersion('8.0', 'php:8.0-cli-alpine', 'appwrite/runtime-for-php:8.0', [System::X86, System::ARM]);
+        $php->addVersion('8.0', 'php:8.0-cli-alpine', 'php-runtime:8.0', [System::X86, System::ARM]);
+        $this->runtimes['php'] = $php;
     }
 
     /**
