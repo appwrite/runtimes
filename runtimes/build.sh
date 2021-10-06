e464c47181eb5be8a7f24771a6904e160004ba10
Bradley Schofield <bradley@appwrite.io>
2021-10-06 23:47:12 +0100

    Add new runtimes to cross compile

--- runtimes/build.sh
+++ runtimes/build.sh
@@ -1,5 +1,8 @@
 echo 'Starting build...'
 
+echo 'Dart 2.12...'
+docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/386 -t dart-runtime:2.12 ./runtimes/dart-2.12/ --push
+
 echo 'Deno 1.14...'
 docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/386 -t deno-runtime:1.14 ./runtimes/deno-1.14/ --push
 
@@ -14,3 +17,18 @@ docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64
 
 echo 'PHP 8...'
 docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 -t php-runtime:8.0 ./runtimes/php-8.0/ --push
+
+echo 'Python 3.8...'
+docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 -t python-runtime:3.8 ./runtimes/python-3.8/ --push
+
+echo 'Python 3.8...'
+docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 -t python-runtime:3.9 ./runtimes/python-3.9/ --push
+
+echo 'Rust 1.55.'
+docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 -t rust-runtime:1.55 ./runtimes/rust-1.55/ --push
+
+echo 'Ubuntu 20.04...'
+docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 -t ubuntu-runtime:20.04 ./runtimes/ubuntu-20.04/ --push
+
+echo 'Alpine 3.13.6...'
+docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 -t alpine-runtime:3.13.6 ./runtimes/alpine-3.13.6/ --push
