49bea9c48437f85893dde67164739d89a0727f2b
Bradley Schofield <bradley@appwrite.io>
2021-10-20 14:14:24 +0100

    Fix permission issue on builtCode

--- tests/Runtimes/RuntimesTest.php
+++ tests/Runtimes/RuntimesTest.php
@@ -33,30 +33,30 @@ class RuntimesTest extends TestCase
             //     'runtime' => 'java-16.0',
             //     'tarname' => 'java-16-0.tar.gz',
             // ],
-            'dart-2.12' => [
-                'code' => $functionsDir . '/dart.tar.gz',
-                'entrypoint' => 'index.dart',
-                'timeout' => 15,
-                'runtime' => 'dart-2.12',
-                'tarname' => 'dart-2-12.tar.gz',
-                'filename' => 'index.dart'
-            ],
-            'dart-2.13' => [
-                'code' => $functionsDir . '/dart.tar.gz',
-                'entrypoint' => 'index.dart',
-                'timeout' => 15,
-                'runtime' => 'dart-2.13',
-                'tarname' => 'dart-2-13.tar.gz',
-                'filename' => 'index.dart'
-            ],
-            'dart-2.14' => [
-                'code' => $functionsDir . '/dart.tar.gz',
-                'entrypoint' => 'index.dart',
-                'timeout' => 15,
-                'runtime' => 'dart-2.14',
-                'tarname' => 'dart-2-14.tar.gz',
-                'filename' => 'index.dart'
-            ],
+            // 'dart-2.12' => [
+            //     'code' => $functionsDir . '/dart.tar.gz',
+            //     'entrypoint' => 'index.dart',
+            //     'timeout' => 15,
+            //     'runtime' => 'dart-2.12',
+            //     'tarname' => 'dart-2-12.tar.gz',
+            //     'filename' => 'index.dart'
+            // ],
+            // 'dart-2.13' => [
+            //     'code' => $functionsDir . '/dart.tar.gz',
+            //     'entrypoint' => 'index.dart',
+            //     'timeout' => 15,
+            //     'runtime' => 'dart-2.13',
+            //     'tarname' => 'dart-2-13.tar.gz',
+            //     'filename' => 'index.dart'
+            // ],
+            // 'dart-2.14' => [
+            //     'code' => $functionsDir . '/dart.tar.gz',
+            //     'entrypoint' => 'index.dart',
+            //     'timeout' => 15,
+            //     'runtime' => 'dart-2.14',
+            //     'tarname' => 'dart-2-14.tar.gz',
+            //     'filename' => 'index.dart'
+            // ],
             'node-14.5' => [
                 'code' => $functionsDir . '/node.tar.gz',
                 'entrypoint' => 'index.js',
@@ -268,6 +268,10 @@ class RuntimesTest extends TestCase
                 stderr: $compressStderr,
                 timeout: 60
             );
+
+            var_dump($compressSuccess);
+            var_dump($compressStdout);
+            var_dump($compressStderr);
 
             $this->assertEquals(true, $compressSuccess);
             $this->assertEquals(true, file_exists($builtCodePath));
